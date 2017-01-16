3ce4e75ddaaffb5173591742478b807d6320c4a1
caixiangning <caixiangning@gmail.com>
2017-01-16 16:58:13 +0800

    提交静态代理示例代码

---
+++ src/main/java/org/codethink/reflection/proxy/statics/CalculateServiceProxy.java
@@ -0,0 +1,46 @@
+package org.codethink.reflection.proxy.statics;
+
+import java.util.Date;
+
+/**
+ * 
+ * 服务器端服务接口代理类
+ * 代理类是通过调用委托类的相关方法来提供特定服务的
+ * 
+ * @author CaiXiangNing
+ * @date 2017年1月16日
+ * @email caixiangning@gmail.com
+ */
+public class CalculateServiceProxy implements CalculateService{
+
+	// 被代理的服务对象
+	private CalculateService calculateService;
+	
+	// 注入被代理服务对象
+	public CalculateServiceProxy(CalculateService calculateService) {
+		super();
+		this.calculateService = calculateService;
+	}
+
+	public Integer add(int param1, int param2) {
+		// TODO Auto-generated method stub
+		// 预处理
+		System.out.println("before calling add!");
+		// 调用被代理的calculateService实例的add方法
+		int result = calculateService.add(param1, param2);
+		// 事后处理
+		System.out.println("after calling add!");
+		return result;
+	}
+
+	public Date getTime() {
+		// TODO Auto-generated method stub
+		// 预处理
+		System.out.println("before calling getTime!");
+		// 调用被代理的calculateService实例的getTime方法
+		Date result = calculateService.getTime();
+		// 事后处理
+		System.out.println("after calling getTime!");
+		return result;
+	}
+}
---
+++ src/main/java/org/codethink/reflection/proxy/statics/StaticProxyTest.java
@@ -0,0 +1,26 @@
+package org.codethink.reflection.proxy.statics;
+
+import org.junit.Test;
+
+/**
+ * 
+ * 静态代理测试类
+ * 
+ * @author CaiXiangNing
+ * @date 2017年1月16日
+ * @email caixiangning@gmail.com
+ */
+public class StaticProxyTest {
+	
+	/**
+	 * 静态代理测试方法
+	 */
+	@Test
+	public void testStaticProxy(){
+		// 创建委托类对象
+		CalculateService calculateService = new CalculateServiceImpl();
+		// 在代理类中注入委托类对象
+		CalculateService calculateServiceProxy = new CalculateServiceProxy(calculateService);
+		System.out.println("运行add(100, 200)程序运行结果：" +calculateServiceProxy.add(100, 200));
+	}
+}
